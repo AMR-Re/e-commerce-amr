e282a0dd9bcd135d220dc2f6a24070b52aa697d5
AMR-Re <amrr8020@gmail.com>
2024-01-25 15:04:27 +0300

    implement ecommerce functionality

--- csp.js
+++ csp.js
@@ -7,6 +7,7 @@ const policies = {
     'https://checkout.stripe.com',
     'https://js.stripe.com',
     'https://maps.googleapis.com',
+    'http://127.0.0.1:3000/',
   ],
   'child-src': ["'self'"],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
